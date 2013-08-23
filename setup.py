870d0eadb5bf56cce20cb0daa80642bc0df9531e
boddmg <boddmg@gmail.com>
2013-08-24 01:28:34 +0800

    修改了py2exe的脚本使之能把config.ini自动拷贝进去了
    Signed-off-by: boddmg <boddmg@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # -*- coding: cp936 -*-
 from distutils.core import setup
 import py2exe
+import shutil  
 
 options = {"py2exe":
     {"compressed": 0, 
      "optimize": 2,
      "bundle_files": 1,
-      "dll_excludes": ["cudart.dll"]
     }
     }
-
 setup(console=['DnsWithEmail_Getip.py'],options = options)
+setup(console=['DnsWithEmail_Uploader.py'],options = options)
+shutil.copyfile("config.ini","dist\config.ini")
